d01b17b8098d9677a131ab326651f47552f8cafb
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-01-11 08:41:25 +0700

    clone with github

---
+++ views/o/member/_option_form.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Users (users)
+ * @var $this AdminController
+ * @var $model Users
+ * @var $form CActiveForm
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2012 Ommu Platform (ommu.co)
+ * @link https://github.com/oMMu/Ommu-Users
+ * @contact (+62)856-299-4114
+ *
+ */
+
+	$cs = Yii::app()->getClientScript();
+$js=<<<EOP
+	$('form[name="gridoption"] :checkbox').click(function(){
+		var url = $('form[name="gridoption"]').attr('action');
+		$.ajax({
+			url: url,
+			data: $('form[name="gridoption"] :checked').serialize(),
+			success: function(response) {
+				$.fn.yiiGridView.update('users-grid', {
+					data: $('form[name="gridoption"]').serialize()
+				});
+				return false;
+			}
+		});
+	});
+EOP;
+	$cs->registerScript('grid-option', $js, CClientScript::POS_END);
+?>
+
+<?php echo CHtml::beginForm(Yii::app()->createUrl($this->route), 'get', array(
+	'name' => 'gridoption',
+));
+$columns   = array();
+$exception = array('id');
+foreach($model->metaData->columns as $key => $val) {
+	if(!in_array($key, $exception)) {
+		$columns[$key] = $key;
+	}
+}
+?>
+<ul>
+	<?php foreach($columns as $val): ?>	<li>
+		<?php echo CHtml::checkBox('GridColumn['.$val.']'); ?>
+		<?php echo CHtml::label($val, 'GridColumn_'.$val); ?>
+	</li>
+	<?php endforeach; ?></ul>
+<div class="clear"></div>
+<?php echo CHtml::endForm(); ?>
