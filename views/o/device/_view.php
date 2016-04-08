d466ed8207c09a39d0f34c975ba36154107afb3b
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-04-09 06:40:41 +0700

    device: add source

---
+++ views/o/device/_view.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * User Devices (user-device)
+ * @var $this DeviceController
+ * @var $data UserDevice
+ * version: 0.0.1
+ *
+ * @author Putra Sudaryanto <putra.sudaryanto@gmail.com>
+ * @copyright Copyright (c) 2016 Ommu Platform (ommu.co)
+ * @created date 9 April 2016, 06:37 WIB
+ * @link http://company.ommu.co
+ * @contect (+62)856-299-4114
+ *
+ */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('publish')); ?>:</b>
+	<?php echo CHtml::encode($data->publish); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('user_id')); ?>:</b>
+	<?php echo CHtml::encode($data->user_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('android_id')); ?>:</b>
+	<?php echo CHtml::encode($data->android_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('creation_date')); ?>:</b>
+	<?php echo CHtml::encode($data->creation_date); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('creation_id')); ?>:</b>
+	<?php echo CHtml::encode($data->creation_id); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('modified_date')); ?>:</b>
+	<?php echo CHtml::encode($data->modified_date); ?>
+	<br />
+
+	<?php /*
+	<b><?php echo CHtml::encode($data->getAttributeLabel('modified_id')); ?>:</b>
+	<?php echo CHtml::encode($data->modified_id); ?>
+	<br />
+
+	*/ ?>
+
+</div>
