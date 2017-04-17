81608831829c82e532698a84c20832364848304f
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-04-17 21:38:11 +0700

    add reinstalldatepicker function in gridview filter

--- models/UserHistoryUsername.php
+++ models/UserHistoryUsername.php
@@ -195,7 +195,7 @@ class UserHistoryUsername extends CActiveRecord
 				'htmlOptions' => array(
 					//'class' => 'center',
 				),
-				'filter' => Yii::app()->controller->widget('zii.widgets.jui.CJuiDatePicker', array(
+				'filter' => Yii::app()->controller->widget('application.components.system.CJuiDatePicker', array(
 					'model'=>$this,
 					'attribute'=>'update_date',
 					'language' => 'en',
