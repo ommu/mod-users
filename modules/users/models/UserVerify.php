f71463d25848b4c9691fbf3f3a3f7b50e6c184ba
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-04-27 10:34:48 +0700

    add account controller source, add and modify useroption, verfy function

--- modules/users/models/UserVerify.php
+++ modules/users/models/UserVerify.php
@@ -322,7 +322,7 @@ class UserVerify extends CActiveRecord
 				'{$site_title}', '{$verify_link}',
 			);
 			$verify_replace = array(
-				Utility::getProtocol().'://'.Yii::app()->request->serverName.$this->module->assetsUrl, $this->user->displayname, SupportMailSetting::getInfo(1, 'mail_contact'),
+				Utility::getProtocol().'://'.Yii::app()->request->serverName.$this->module->assetsUrl, $this->user->displayname, SupportMailSetting::getInfo('mail_contact'),
 				$setting->site_title, Utility::getProtocol().'://'.Yii::app()->request->serverName.Yii::app()->createUrl('users/verify/code',array('key'=>$this->code, 'secret'=>$this->user->salt)),
 			);
 			$verify_template = 'user_verify_email';
