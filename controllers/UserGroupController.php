911ff2777c937670c7ef54e33bfac807658e7a1d
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-05-18 11:34:30 +0700

    drop .bak

--- controllers/UserGroupController.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace ommu\users\controllers;
-
-class UserGroupController extends \app\components\Controller
-{
-	public function actionIndex() {
-		return $this->render('index');
-	}
-}
