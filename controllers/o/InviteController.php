3f8e4b1022ce22db7c3048b34e6a5e88955c6864
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-25 11:29:39 +0700

    commit

--- controllers/o/InviteController.php
+++ controllers/o/InviteController.php
@@ -29,6 +29,8 @@
 
 class InviteController extends Controller
 {
+	use FileTrait;
+	
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
