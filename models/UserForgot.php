5335f68b3c1581407c4e859f57fa3dfbea844a80
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2021-08-10 18:26:17 +0700

    commit

--- models/UserForgot.php
+++ models/UserForgot.php
@@ -35,6 +35,7 @@ use Yii;
 use yii\helpers\Html;
 use yii\helpers\Url;
 use app\components\helpers\TimeHelper;
+use yii\validators\EmailValidator;
 
 class UserForgot extends \app\components\ActiveRecord
 {
@@ -301,7 +302,7 @@ class UserForgot extends \app\components\ActiveRecord
 	{
         if (parent::beforeValidate()) {
             if ($this->isNewRecord) {
-				$validator = new \yii\validators\EmailValidator();
+				$validator = new EmailValidator();
                 if ($this->scenario == self::SCENARIO_WITH_FORM && $validator->validate($this->email_i) === true && $this->user_id == null) {
 					$user = Users::find()
 						->select(['user_id'])
